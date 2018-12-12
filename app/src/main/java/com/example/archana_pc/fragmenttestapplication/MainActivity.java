e3048f7018ccf4176d5c791b20de0135886eb83a
Archana Chauhan <archanachauhan920@gmail.com>
2018-12-12 23:07:18 +0530

    initial commit

---
+++ app/src/main/java/com/example/archana_pc/fragmenttestapplication/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.archana_pc.fragmenttestapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
